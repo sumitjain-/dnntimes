f60b31b2e9c7f30d74136a17df7a4c2107eda5c7
sumitjain- <sumeetsuniljain@gmail.com>
2013-08-19 16:38:32 +0530

    Local build

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -39,8 +39,45 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
-        
+        navigator.notification.alert("Reached here !!");
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"470915515710","ecb":"app.onNotificationGCM"});
         
         console.log('Received Event: ' + id);
     }
+    
+    successHandler: function(result) {
+    alert('Callback Success! Result = '+result)
+    },
+    
+    errorHandler:function(error) {
+    alert(error);
+    },
+        
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
+    }
+    
 };
--- platforms/android/bin/DNNTimes-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/sumeetjain/Desktop/dnntimes/platforms/android/bin/DNNTimes-debug-unaligned.apk : \
-/Users/sumeetjain/Desktop/dnntimes/platforms/android/bin/DNNTimes.ap_ \
-/Users/sumeetjain/Desktop/dnntimes/platforms/android/bin/classes.dex \
-/Users/sumeetjain/Desktop/dnntimes/platforms/android/libs/cordova-dev.jar \
