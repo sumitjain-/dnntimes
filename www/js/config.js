e5862e1f6478ee7a2829228199768b18e0f5443a
sumitjain- <sumeetsuniljain@gmail.com>
2013-08-19 15:23:02 +0530

    Day 2 - updated www

---
+++ www/js/config.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    config = new Object();
+    config.serverUrl = 'http://dnn-times.com/';
+    
+});
+
+$.ajaxSetup({
+    "error":function(){ 
+        $("#notif_display").append('<li>No network</li>');
+    }
+});
---
+++ www/js/get.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $.getJSON(config.serverUrl+'public_lib/json_news_50', function(data){
+        var no_of_notif = data.length;
+        
+        for(i=0 ; i < no_of_notif ; i++){
+            $('#notif_display').append('<li><a class="" id="notif" href="#post" data-transition="slide" onclick="get_post('+data[i].post_id+')">'+ data[i].dnn_post_title +'</a></li>');
+            console.log(data[i].dnn_post_title);
+        }
+        $('#notif_display').listview('refresh');
+        //var i=1 ;
+        console.log(data);
+        
+    }).error(function(){
+        alert("Check internet connection and re-open the app..");
+    });
+    
+});
