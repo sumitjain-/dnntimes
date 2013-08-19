0dcbbb2622c7cd2aa32590294bda1168a4545302
sumitjain- <sumeetsuniljain@gmail.com>
2013-08-19 16:01:31 +0530

    GCM test 1

--- www/js/index.js
+++ www/js/index.js
@@ -40,7 +40,44 @@ var app = {
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"51975193249","ecb":"app.onNotificationGCM"});
         
         console.log('Received Event: ' + id);
     }
+    
+    successHandler: function(result) {
+    alert('Callback Success! Result = '+result)
+    },
+    
+    errorHandler:function(error) {
+    alert(error);
+    },
+        
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
+    }
+    
 };
