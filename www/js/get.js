309f84efe8fed28475c5abc4ed9fa6e67901d997
sumitjain- <sumeetsuniljain@gmail.com>
2013-08-20 10:31:32 +0530

    Responsive styles

--- www/js/get.js
+++ www/js/get.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     $.getJSON(config.serverUrl+'public_lib/json_news_50', function(data){
         var no_of_notif = data.length;
         
+        $('#notif_display').html("");
+        
         for(i=0 ; i < no_of_notif ; i++){
             $('#notif_display').append('<li><a class="" id="notif" href="#post" data-transition="slide" onclick="get_post('+data[i].post_id+')">'+ data[i].dnn_post_title +'</a></li>');
             console.log(data[i].dnn_post_title);
